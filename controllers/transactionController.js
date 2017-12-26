e578a3e91e05ad2ef6cde07dd46f15ead344007d
Irianto Kbarek <irianto223@gmail.com>
2017-12-26 15:40:59 +0900

    create transaction init

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const Transaction = require('../models/transactionModel')
 
 module.exports = {
@@ -5,5 +6,14 @@ module.exports = {
     Transaction.find()
     .then(result => res.send(result))
     .catch(err => res.send(err))
+  },
+  create: (req, res) => {
+    var distance = 0
+    axios({
+      method: 'GET',
+      url: `https://maps.googleapis.com/maps/api/distancematrix/json?origins=-2.557137,140.704436&destinations=-2.557847,140.709090&mode=car&key=${process.env.GOOGLE_API_KEY}`
+    })
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
   }
 }
